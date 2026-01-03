5bcb11e3f6d52d89bc21639c9f8a9a350de397c8
Nick Reynolds <ncmreynolds@googlemail.com>
2026-01-03 17:08:44 +0000

    Tidy up example

--- examples/blink/blink.ino
+++ examples/blink/blink.ino
@@ -19,19 +19,27 @@ uint32_t loopTimer = 0; //Use a loop timer instead of delay
 bool ledState = true;  //Track LED state
 
 void setup() {
-  Serial.begin(115200);   //Set up the Serial for outpur
-  while(!Serial){}        //Wait for Serial to start, only needed on some boards
+  Serial.begin(115200);     //Set up the Serial for outpur
+  while(!Serial){}          //Wait for Serial to start, only needed on some boards
   //charger.debug(Serial);  //Enable (quite verbose) debug output for the charger. Necessary for print_registers() to work
-  Wire.begin();           //Start I²C
-  charger.begin();        //Start the charger
-  if(charger.set_pg_pin_mode(BQ25186_PG_PIN_MODE_GPO)) {
-    Serial.println("PG Pin set as output");
+  delay(5000);              //Give a USB connection time to come up
+  Wire.begin();             //Start I²C
+  if(charger.begin()) {     //Start the charger
+    Serial.println("Read charger configuration OK");
+    if(charger.set_pg_pin_mode(BQ25186_PG_PIN_MODE_GPO)) {
+      Serial.println("PG Pin set as output");
+    } else {
+      Serial.println("Unable to set PG Pin as output");
+    }
+  } else {
+    Serial.println("Unable to read charger registers, is it connected?");
   }
 }
 
 void loop() {
   if(millis() - loopTimer > 1e3) { //Query the charger every 10s
     loopTimer = millis();           //Avoiding using delay()
+    Serial.print("LED ");
     if(ledState == true) {
       ledState = false;
       if(charger.set_pg_pin_state(BQ25186_PG_GPO_HIGH_IMPEDENCE)) {
