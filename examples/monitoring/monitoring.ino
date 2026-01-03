6aacfede331424a92ac1ea98c3493fbb76992901
Nick Reynolds <ncmreynolds@googlemail.com>
2026-01-03 17:26:37 +0000

    Tidy example

--- examples/monitoring/monitoring.ino
+++ examples/monitoring/monitoring.ino
@@ -6,9 +6,6 @@
  * SDA 8
  * SCL 9
  *
- *
- *
- *
  */
 
 #include "Wire.h" //Include the I²C library
@@ -18,30 +15,37 @@ bq25186 charger;  //Create a new instance of the charger object
 uint32_t loopTimer = 0; //Use a loop timer instead of delay
 
 void setup() {
-  Serial.begin(115200);   //Set up the Serial for outpur
-  while(!Serial){}        //Wait for Serial to start, only needed on some boards
+  Serial.begin(115200);     //Set up the Serial for output
+  while(!Serial){}          //Wait for Serial to start, only needed on some boards
+  delay(5000);              //Give a USB connection time to come up
   //charger.debug(Serial);  //Enable (quite verbose) debug output for the charger. Necessary for print_registers() to work
-  Wire.begin();           //Start I²C
-  charger.begin();        //Start the charger
+  Wire.begin();             //Start I²C
+  if(charger.begin()) {     //Start the charger
+    Serial.println("Read charger configuration OK");
+  } else {
+    Serial.println("Unable to read charger registers, is it connected?");
+  }
 }
 
 void loop() {
   if(millis() - loopTimer > 10e3) { //Query the charger every 10s
     loopTimer = millis();           //Avoiding using delay()
     //Power good
-    Serial.print("\r\nPower in good:");
-    printTrueFalse(charger.vin_pgood());
+    Serial.print("\r\nCharging\r\nPower in good:");
+    printTrueFalse(charger.vin_pgood_stat() == BQ25186_POWER_GOOD);
     Serial.print("\t");
     //VIN monitoring
-    Serial.print("Charging voltage low threshold:");
+    Serial.print("Charging VINDPM low threshold:");
     if(charger.get_vindpm() == BQ25186_VINDPM_DISABLED) {
       Serial.print("disabled");
     } else if(charger.get_vindpm() == BQ25186_VINDPM_4_7) {
-      Serial.print("4.7v");
+      Serial.print("4.7V");
     } else if(charger.get_vindpm() == BQ25186_VINDPM_4_5) {
-      Serial.print("4.5v");
+      Serial.print("4.5V");
     } else if(charger.get_vindpm() == BQ25186_VINDPM_VBAT_PLUS_300) {
       Serial.print("battery+300mV");
+    } else {
+      Serial.print("unknown");
     }
     Serial.print("\t");
     //Charging enabled
@@ -50,109 +54,148 @@ void loop() {
     Serial.print("\t");
     //Charging
     Serial.print("Charging state:");
-    if(charger.chg() == BQ25186_ENABLED_BUT_NOT_CHARGING) {
+    if(charger.chg_stat() == BQ25186_ENABLED_BUT_NOT_CHARGING) {
       Serial.println("Enabled but not charging");
-    } else if(charger.chg() == BQ25186_CC_CHARGING) {
+    } else if(charger.chg_stat() == BQ25186_CC_CHARGING) {
       Serial.println("Constant current charging");
-    } else if(charger.chg() == BQ25186_CV_CHARGING) {
+    } else if(charger.chg_stat() == BQ25186_CV_CHARGING) {
       Serial.println("Constant voltage charging");
-    } else if(charger.chg() == BQ25186_CHARGING_DONE_OR_DISABLED) {
+    } else if(charger.chg_stat() == BQ25186_CHARGING_DONE_OR_DISABLED) {
       Serial.println("Done or disabled");
+    } else {
+      Serial.print("unknown");
     }
-    //Cell voltage
-    Serial.print("Cell regulated voltage:");
+    //Battery voltage
+    Serial.print("\r\nBattery\r\nRegulated voltage:");
     Serial.print(charger.get_vbatreg());
     Serial.print("v\t");
     //Charging current
-    Serial.print("Cell charge current:");
+    Serial.print("Charge current:");
     Serial.print(charger.get_ichg());
     Serial.print("mA\t");
     //Termination current
-    Serial.print("Cell termination charge current:");
+    Serial.print("Termination charge current:");
     if(charger.get_iterm() == BQ25186_ITERM_20_PERCENT) {
-      Serial.print("20%\t");
+      Serial.print("20%");
     } else if(charger.get_iterm() == BQ25186_ITERM_10_PERCENT) {
-      Serial.print("10%\t");
+      Serial.print("10%");
     } else if(charger.get_iterm() == BQ25186_ITERM_5_PERCENT) {
-      Serial.print("5%\t");
+      Serial.print("5%");
     } else if(charger.get_iterm() == BQ25186_ITERM_DISABLE) {
-      Serial.print("disabled\t");
+      Serial.print("disabled");
+    } else {
+      Serial.print("unknown");
     }
+    Serial.print("\t");
     //Fast charge mode
     Serial.print("Fast charge mode:");
-    printTrueFalse(charger.get_en_fc_mode());
-    Serial.print("\t");
+    printEnabledDisabledLn(charger.get_en_fc_mode());
     //Discharge current limit
-    Serial.print("Battery discharge over current protection limit:");
-    Serial.print(charger.get_ibat_ocp());
-    Serial.println("mA\t");
+    Serial.print("\r\nProtection\r\nDischarge over current protection limit:");
+    if(charger.get_ibat_ocp() == BQ25186_IBAT_OCP_500MA) {
+      Serial.print(500);
+    } else if(charger.get_ibat_ocp() == BQ25186_IBAT_OCP_1000MA) {
+      Serial.print(1000);
+    } else if(charger.get_ibat_ocp() == BQ25186_IBAT_OCP_1500MA) {
+      Serial.print(1500);
+    } else if(charger.get_ibat_ocp() == BQ25186_IBAT_OCP_3000MA) {
+      Serial.print(3000);
+    } else {
+      Serial.print("unknown ");
+    }
+    Serial.print("mA\t");
     //Battery undervoltage lockout threshold
     Serial.print("Battery undervoltage lockout threshold:");
     Serial.print(charger.get_buvlo());
-    Serial.print("v\t");
+    Serial.println("v\t");
     //Button actions
-    Serial.print("Button long press time:");
-    Serial.print(charger.get_long_press_time());
+    Serial.print("\r\nButton\r\nPush:");
+    printEnabledDisabled(charger.get_en_push() == BQ25186_PUSH_ENABLED);
+    Serial.print("\tLong press time:");
+    if(charger.get_long_press_time() == BQ25186_MR_LPRESS_5S) {
+      Serial.print(5);
+    } else if(charger.get_long_press_time() == BQ25186_MR_LPRESS_10S) {
+      Serial.print(10);
+    } else if(charger.get_long_press_time() == BQ25186_MR_LPRESS_15S) {
+      Serial.print(15);
+    } else if(charger.get_long_press_time() == BQ25186_MR_LPRESS_20S) {
+      Serial.print(20);
+    } else {
+      Serial.print("unknown ");
+    }
     Serial.print("s\t");
-    Serial.print("Button long press action:");
+    Serial.print("Long press action:");
     if(charger.get_long_press_action() == BQ25186_PB_LPRESS_ACTION_NOTHING) {
-      Serial.print("nothing\t");
+      Serial.print("nothing");
     } else if(charger.get_long_press_action() == BQ25186_PB_LPRESS_ACTION_RESET) {
-      Serial.print("reset\t");
+      Serial.print("reset");
     } else if(charger.get_long_press_action() == BQ25186_PB_LPRESS_ACTION_SHIP) {
-      Serial.print("ship mode\t");
+      Serial.print("ship mode");
     } else if(charger.get_long_press_action() == BQ25186_PB_LPRESS_ACTION_SHUTDOWN) {
-      Serial.print("shutdown mode\t");
+      Serial.print("shutdown mode");
+    } else {
+      Serial.print("unknown");
     }
     Serial.println();
     //System voltage regulation
-    Serial.print("System regulated voltage:");
-    if(charger.get_sys_regulation_voltgage() == BQ25186_SYS_REG_CTRL_BATTERY_TRACK) {
-      Serial.print("track battery\t");
-    } else if(charger.get_sys_regulation_voltgage() == BQ25186_SYS_REG_CTRL_4_4V) {
-      Serial.print("4.4V\t");
-    } else if(charger.get_sys_regulation_voltgage() == BQ25186_SYS_REG_CTRL_4_5V) {
-      Serial.print("4.5V\t");
-    } else if(charger.get_sys_regulation_voltgage() == BQ25186_SYS_REG_CTRL_4_6V) {
-      Serial.print("4.6V\t");
-    } else if(charger.get_sys_regulation_voltgage() == BQ25186_SYS_REG_CTRL_4_7V) {
-      Serial.print("4.7V\t");
-    } else if(charger.get_sys_regulation_voltgage() == BQ25186_SYS_REG_CTRL_4_8V) {
-      Serial.print("4.8V\t");
-    } else if(charger.get_sys_regulation_voltgage() == BQ25186_SYS_REG_CTRL_4_9V) {
-      Serial.print("4.9V\t");
-    } else if(charger.get_sys_regulation_voltgage() == BQ25186_SYS_REG_CTRL_PASS_THROUGH) {
-      Serial.print("pass through\t");
+    Serial.print("\r\nSystem\r\nRegulated voltage:");
+    if(charger.get_sys_regulation_voltage() == BQ25186_SYS_REG_CTRL_BATTERY_TRACK) {
+      Serial.print("track battery");
+    } else if(charger.get_sys_regulation_voltage() == BQ25186_SYS_REG_CTRL_4_4V) {
+      Serial.print("4.4V");
+    } else if(charger.get_sys_regulation_voltage() == BQ25186_SYS_REG_CTRL_4_5V) {
+      Serial.print("4.5V");
+    } else if(charger.get_sys_regulation_voltage() == BQ25186_SYS_REG_CTRL_4_6V) {
+      Serial.print("4.6V");
+    } else if(charger.get_sys_regulation_voltage() == BQ25186_SYS_REG_CTRL_4_7V) {
+      Serial.print("4.7V");
+    } else if(charger.get_sys_regulation_voltage() == BQ25186_SYS_REG_CTRL_4_8V) {
+      Serial.print("4.8V");
+    } else if(charger.get_sys_regulation_voltage() == BQ25186_SYS_REG_CTRL_4_9V) {
+      Serial.print("4.9V");
+    } else if(charger.get_sys_regulation_voltage() == BQ25186_SYS_REG_CTRL_PASS_THROUGH) {
+      Serial.print("pass through");
+    } else {
+      Serial.print("unknown");
     }
+    Serial.print("\t");
     //System mode
-    Serial.print("System power mode:");
+    Serial.print("Power mode:");
     if(charger.get_sys_mode() == BQ25186_SYS_MODE_VIN_OR_VBAT) {
-      Serial.print("Charger input or cell\t");
+      Serial.print("Charger input or battery\t");
     } else if(charger.get_sys_mode() == BQ25186_SYS_MODE_VBAT_ONLY) {
-      Serial.print("cell only\t");
+      Serial.print("Battery only\t");
     } else if(charger.get_sys_mode() == BQ25186_SYS_MODE_FLOAT) {
       Serial.print("disconnected and floating\t");
     } else if(charger.get_sys_mode() == BQ25186_SYS_MODE_PULLDOWN) {
       Serial.print("disconnected and pulled down\t");
+    } else {
+      Serial.print("unknown");
     }
     //I2C watchdog
-    Serial.print("I2C system watchdog:");
-    if(charger.get_i2c_watchdog_mode() == BQ25186_SYS_WATCHDOG_15S_ENABLE) {
-      Serial.println("enabled\t");
-    } else {
-      Serial.println("disabled\t");
-    }
-    Serial.println();
+    Serial.print("I2C system startup watchdog:");
+    printEnabledDisabledLn(charger.get_i2c_watchdog_mode() == BQ25186_SYS_WATCHDOG_15S_ENABLE);
   }
 }
 void printTrueFalse(bool value) {
   if(value) {
-    Serial.print("true ");
+    Serial.print("true");
   } else {
     Serial.print("false");
   }
 }
 void printTrueFalseLn(bool value) {
   printTrueFalse(value);
+  Serial.println();
+}
+void printEnabledDisabled(bool value) {
+  if(value) {
+    Serial.print("enabled");
+  } else {
+    Serial.print("disabled");
+  }
+}
+void printEnabledDisabledLn(bool value) {
+  printEnabledDisabled(value);
   Serial.println();
 }
